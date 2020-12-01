4f8d90ab05a8a2123334f54883f329413c6dc480
simon <parkseungchul@gmail.com>
2020-12-01 15:33:12 +0900

    j201

--- j201/src/main/java/com/psc/j201/J201Application.java
+++ j201/src/main/java/com/psc/j201/J201Application.java
@@ -1,13 +1,40 @@
 package com.psc.j201;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
-public class J201Application {
+public class J201Application implements ApplicationListener<ApplicationReadyEvent>{
 
+	@Value("${server.message}")
+	String message;
+	
+	@Value("${server.codesN}")
+	List codesN;
+	
+	@Value("${server.codesA}")
+	List codesA;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(J201Application.class, args);
 	}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		System.out.println(message);
+		System.out.println(codesN.size() +" "+codesN.toString());
+		System.out.println(codesA.size() +" "+codesA.toString());
+		
+	}
+	
+	
 
 }
