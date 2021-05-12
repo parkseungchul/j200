4c314fccfbb2cf1b9761d04bd1a344c3c54ae99e
simon <parkseungchul@gmail.com>
2021-05-12 17:26:11 +0900

    j200 정리

---
+++ j205/gateway/src/test/java/com/psc/cloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    voidcontextLoads() {
+    }
+
+}
