9b59ce34a72e79523ac476a4dddd448bcb059909
simon <parkseungchul@gmail.com>
2021-05-12 17:26:44 +0900

    j200 정리

---
+++ j205/biz/src/main/java/com/psc/cloud/biz/BizApplication.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizApplication.class, args);
+    }
+
+}
---
+++ j205/biz/src/test/java/com/psc/cloud/biz/BizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
