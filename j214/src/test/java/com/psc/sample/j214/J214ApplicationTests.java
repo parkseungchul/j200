b0e68f845c0d9eb27e5c223ccd8b290b0ed5ac02
simon <parkseungchul@gmail.com>
2022-04-18 10:19:16 +0900

    214

---
+++ j214/src/test/java/com/psc/sample/j214/J214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.sample.j214;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
