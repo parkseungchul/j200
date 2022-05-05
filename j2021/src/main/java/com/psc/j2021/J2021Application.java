f3e8e896043a860e04f3d15663f10eac526edb13
simon <parkseungchul@gmail.com>
2022-05-06 01:41:52 +0900

    db adapter

---
+++ j2021/src/main/java/com/psc/j2021/J2021Application.java
@@ -0,0 +1,15 @@
+package com.psc.j2021;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class J2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2021Application.class, args);
+    }
+
+}
---
+++ j2021/src/test/java/com/psc/j2021/J2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.j2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
